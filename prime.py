5c66d9c83f6902d7ad8edf50920a8b35012defcd
1998fx <106678831+1998ky262@users.noreply.github.com>
2023-04-23 17:17:22 +0900

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,25 @@
+import math
+from matplotlib import pyplot as pyp
+def prime_tf(num):
+    if num <= 1:
+        return False
+    if num == 2:
+        return True
+    a = math.ceil(math.sqrt(num))
+    for i in range(2, a+1):
+        if num % i == 0:
+            return False
+    return True
+z=0
+data=[]
+data2=[]
+for b in range(50):
+    data2.append(z)
+    count = 0
+    for c in range(1000):
+        z+=1
+        if(prime_tf(z)):
+            count+=1
+    data.append(count)
+pyp.plot(data2,data,marker="o")
+pyp.show()
